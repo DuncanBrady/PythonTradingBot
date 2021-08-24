a02d8521ed6b07e47316335106aa17ae992e0ac4
Luke Banicevic <60857954+banaboi@users.noreply.github.com>
2021-08-24 17:53:58 +1000

    Fixed unit tests, code now works

--- tests/bot_test.py
+++ tests/bot_test.py
@@ -7,13 +7,11 @@ from src.Bot import Bot
 
 class TestBot(unittest.TestCase):
     
-    def setUp(self):
-        self.bot = Bot(balance=1000)
-    
     def test_buy(self):
-        self.bot.buy("EXR", 1.00, 500)
-        self.assertEqual(self.bot.get_balance(), 500)
-        self.assertEqual(self.bot.get_position(), [{
+        bot = Bot(balance = 1000, position = [])
+        bot.buy("EXR", 1.00, 500)
+        self.assertEqual(bot.get_balance(), 500)
+        self.assertEqual(bot.get_position(), [{
             "code" : "EXR",
             "value" : 1.00,
             "num_shares" : 500,
@@ -21,8 +19,10 @@ class TestBot(unittest.TestCase):
         }])
 
     def test_sell(self):
-        self.bot.sell("EXR", 2.00)
-        self.assertEqual(self.bot.get_balance(), 1500)
+        bot = Bot(balance = 1000, position = [])
+        bot.buy("EXR", 1.00, 500)
+        bot.sell("EXR", 2.00)
+        self.assertEqual(bot.balance, 1500)
 
 
 
