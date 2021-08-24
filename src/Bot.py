48183182e6137b488fd38497184e460924e908ac
Luke Banicevic <60857954+banaboi@users.noreply.github.com>
2021-08-24 14:33:39 +1000

    Unit tests are sort of working, getters and setters causing issues

--- src/Bot.py
+++ src/Bot.py
@@ -24,18 +24,27 @@ class Bot:
         self.position = position
     
     @staticmethod
-    def create_position_object(code, price, num_shares):
+    def create_position_object(code, price, money_invested):
         return {
             "code" : code,
             "value" : price,
-            "num_shares" : num_shares
+            "num_shares" : money_invested / price,
+            "total_invested" : money_invested
         }
+    @staticmethod
+    def update_position_object(share_object, price, money_invested):
+        share_object['total_invested'] += money_invested
+        share_object['num_shares'] += money_invested / price
+        share_object['value'] = share_object['total_invested'] / share_object['num_shares']
     
     def set_stop_loss(self, stop_loss):
         self.stop_loss = stop_loss
 
     def set_balance(self, balance):
         self.balance = balance
+        
+    def set_position(self, position):
+        self.position = position
     
     def get_balance(self):
         return self.balance
@@ -46,18 +55,26 @@ class Bot:
     def get_position(self):
         return self.position
     
-    def buy(self, code, price, num_shares):
-        if self.balance - price * num_shares < 0:
+    def buy(self, code, price, money_invested):
+        if self.balance - money_invested < 0:
             raise Exception("Not enough money to buy")
 
-        self.set_balance(self.get_balance() - (price * num_shares))
-        self.position.append(self.create_position_object(code,price,num_shares))    
-
-    def sell(self, code, sell_price):
+        self.set_balance(self.get_balance() - money_invested)
         
+        # if the code is already in the list, try and find it
+        share_object = [x for x in self.get_position() if x['code'] == code]
+        
+        if len(share_object) == 0:
+            self.position.append(self.create_position_object(code,price,money_invested))  
+        else:
+            self.update_position_object(share_object[0], price, money_invested)
+        
+        
+    def sell(self, code, sell_price):
         sell_object = [x for x in self.get_position() if x['code'] == code][0]
-        self.set_balance(self.get_balance + sell_price * sell_object['num_shares'])
+        self.set_balance(self.get_balance() + sell_price * sell_object['num_shares'])
         self.position.remove(sell_object)
+        
     
     
     
--- tests/bot_test.py
+++ tests/bot_test.py
@@ -12,13 +12,17 @@ class TestBot(unittest.TestCase):
     
     def test_buy(self):
         self.bot.buy("EXR", 1.00, 500)
-        self.assertEquals(self.bot.get_balance(), 500)
-        self.assertEquals(self.bot.get_position(), [{
+        self.assertEqual(self.bot.get_balance(), 500)
+        self.assertEqual(self.bot.get_position(), [{
             "code" : "EXR",
             "value" : 1.00,
-            "num_shares" : 500
+            "num_shares" : 500,
+            "total_invested" : 500
         }])
 
+    def test_sell(self):
+        self.bot.sell("EXR", 2.00)
+        self.assertEqual(self.bot.get_balance(), 1500)
 
 
 
