5480e0855b051ee283b0f70a4d250f5f0717264f
Luke Banicevic <60857954+banaboi@users.noreply.github.com>
2021-08-30 13:22:53 +1000

    Added docstrings and refactored

--- src/StatBot.py
+++ src/StatBot.py
@@ -10,14 +10,28 @@ Author: Robert Brady & Luke Banicevic
 class StatBot:
 
     def __init__(self, mv_avg = {}, RSI = {}, past_prices = {}, codes = []):
+        """Constructor for the StatBot class
+
+        Args:
+            mv_avg (dict, optional): Stores the moving averages for all stock codes. Defaults to {}.
+            RSI (dict, optional): Stores the RSI's for all stock codes . Defaults to {}.
+            past_prices (dict, optional): Stores the past prices of the monitored stock codes. Defaults to {}.
+            codes (list, optional): Stock codes which are set to be monitored. Defaults to [].
+        """
         self.mv_avg = mv_avg
         self.past_prices = past_prices
         self.RSI = RSI
         self.codes
         if codes is not None:
-            for key in codes:
-                self.mv_avg[str(key)] = 0.0
-                self.past_prices[str(key)] = []
-                self.RSI[str(key)] = 0.0
+            self.build_dicts(codes)
 
-    def          
+    def build_dicts(self, codes): 
+        """Builds the statsbots collection of data as stock codes as keys
+
+        Args:
+            codes ([type]): [description]
+        """
+        for key in codes:
+            self.mv_avg[str(key)] = 0.0
+            self.past_prices[str(key)] = []
+            self.RSI[str(key)] = 0.0   
