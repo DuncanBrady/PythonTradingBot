c8fd05c8f8ac659dd290c4085a4ad0585f869c48
DuncanBrady <robert.duncan.brady@gmail.com>
2021-08-30 13:29:25 +1000

    added test file for statbot with basic constructor test
    Please for the love of god work you cunt

--- src/StatBot.py
+++ src/StatBot.py
@@ -6,35 +6,18 @@
 Written: August 2021
 Author: Robert Brady & Luke Banicevic 
 '''
-import numpy as np
-import scipy as sp
-
 
 class StatBot:
 
     def __init__(self, mv_avg = {}, RSI = {}, past_prices = {}, codes = []):
-        """Constructor for the StatBot class
-
-        Args:
-            mv_avg (dict, optional): Stores the moving averages for all stock codes. Defaults to {}.
-            RSI (dict, optional): Stores the RSI's for all stock codes . Defaults to {}.
-            past_prices (dict, optional): Stores the past prices of the monitored stock codes. Defaults to {}.
-            codes (list, optional): Stock codes which are set to be monitored. Defaults to [].
-        """
         self.mv_avg = mv_avg
         self.past_prices = past_prices
         self.RSI = RSI
         self.codes
         if codes is not None:
-            self.build_dicts(codes)
+            for key in codes:
+                self.mv_avg[str(key)] = 0.0
+                self.past_prices[str(key)] = []
+                self.RSI[str(key)] = 0.0
 
-    def build_dicts(self, codes): 
-        """Builds the statsbots collection of data as stock codes as keys
-
-        Args:
-            codes (list[string]): List of Stock codes which are being monitored
-        """
-        for key in codes:
-            self.mv_avg[str(key)] = 0.0
-            self.past_prices[str(key)] = []
-            self.RSI[str(key)] = 0.0   
+    def          
