c22e73a37340bb6097e90d29b7887fe14cd44a8c
DuncanBrady <robert.duncan.brady@gmail.com>
2021-08-30 13:35:42 +1000

    editing incorrect commit message

--- tests/Statbot_test.py
+++ tests/Statbot_test.py
@@ -1,1 +1,12 @@
-import unit
+import unittest
+import sys
+sys.path.append("..")
+
+from src.StatBot import StatBot
+
+class TestStatBot(unittest.TestCase):
+
+    def test_construtor(self):
+        stat = StatBot(codes=["LUKE", "is", "gay"])
+
+    
