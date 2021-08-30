8aeb0b7593695cc7b57e4fd62c905c69ff9aa4be
Luke Banicevic <60857954+banaboi@users.noreply.github.com>
2021-08-30 13:22:53 +1000

    Added docstrings and refactored

--- src/StatBot.py
+++ src/StatBot.py
@@ -13,37 +13,28 @@ import scipy as sp
 class StatBot:
 
     def __init__(self, mv_avg = {}, RSI = {}, past_prices = {}, codes = []):
+        """Constructor for the StatBot class
+
+        Args:
+            mv_avg (dict, optional): Stores the moving averages for all stock codes. Defaults to {}.
+            RSI (dict, optional): Stores the RSI's for all stock codes . Defaults to {}.
+            past_prices (dict, optional): Stores the past prices of the monitored stock codes. Defaults to {}.
+            codes (list, optional): Stock codes which are set to be monitored. Defaults to [].
+        """
         self.mv_avg = mv_avg
         self.past_prices = past_prices
         self.RSI = RSI
         self.codes
         if codes is not None:
-            for key in codes:
-                self.mv_avg[str(key)] = 0.0
-                self.past_prices[str(key)] = {"open" : [], "high" : [], "close" : []}
-                self.RSI[str(key)] = 0.0
+            self.build_dicts(codes)
 
-    def calc_bands(self, mv_avg):
-        pass
-    
-    def rsi_calc(self):
-        pass
+    def build_dicts(self, codes): 
+        """Builds the statsbots collection of data as stock codes as keys
 
-    def update_code(self, code, incoming_data):
-        pass
-
-    def process_incoming(self, incoming_data):
-        for key in incoming_data:
-            update_code(str(key), incoming_data[key])
-        return incoming_data
-
-    def set_mv_avg(self, code, mv_avg):
-        self.mv_avg[code] = mv_avg
-
-    def set_rsi(self, code, rsi):
-        if self.rsi.get(code):
-            self.rsi[code] = rsi 
-   
-    def get_mv_avg(self, code):
-        return self.mv_avg.get(code, None)
-    
+        Args:
+            codes ([type]): [description]
+        """
+        for key in codes:
+            self.mv_avg[str(key)] = 0.0
+            self.past_prices[str(key)] = []
+            self.RSI[str(key)] = 0.0   
