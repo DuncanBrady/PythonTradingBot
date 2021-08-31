f850e92ebab6a8c90eb91316b13d5d6e4f9236fd
DuncanBrady <robert.duncan.brady@gmail.com>
2021-08-31 12:30:43 +1000

    Added basic constructor test in StatBot_test.py, additional getters added to Statbot.py for RSI and past prices

--- tests/Statbot_test.py
+++ tests/Statbot_test.py
@@ -6,7 +6,14 @@ from src.StatBot import StatBot
 
 class TestStatBot(unittest.TestCase):
 
-    def test_construtor(self):
-        stat = StatBot(codes=["TST", "ISA", "EXR"])
+	def test_construtor(self):
+		stckcodes = ["TST", "ISA", "EXR"]
+		stat = StatBot(codes=stckcodes)
+		for code in stckcodes:
+			self.assertEqual(stat.get_mv_avg(code), 0.0)
+			self.assertEqual(stat.get_rsi(code), 0.0)
+			self.assertEqual(stat.get_price(code), {"open": [], "high":[], "close": []})
 
-    
+
+if __name__ == '__main__':
+	unittest.main()  
