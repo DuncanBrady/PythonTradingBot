6c87950f5bb55a32ead423874eaa3078cbc7bc02
Luke Banicevic <60857954+banaboi@users.noreply.github.com>
2021-08-31 13:34:32 +1000

    Added update functions for mv avgs and prices, yet to test

--- src/StatBot.py
+++ src/StatBot.py
@@ -46,23 +46,63 @@ class StatBot:
         
         
         #retrieve the past prices of the stock
-
+        past_prices = self.get_price(code)
+            
         #calculate the standard deviation
+        std = np.std(np.array(past_prices['close']))
 
-        #retrive the moving average
+        #retrieve the moving average
+        mv_avg = self.get_mv_avg(code)
 
         #return tuple of upper and lower values
+        return (mv_avg - 2*std, mv_avg + 2*std)
 
     def rsi_calc(self):
         pass
 
     def update_code(self, code, incoming_data):
-        pass
+        """Updates the information for a stock code given incoming new data
+
+        Args:
+            code (string): stock code of the stock in question
+            incoming_data (json/dict): object containing stock codes as keys and prices objects as values
+        """
+        
+        price_object = self.get_price(code)
+        self.update_prices(price_object, incoming_data[code])
+        self.update_mv_avg(code)
+        
+    @staticmethod
+    def update_prices(old_prices, new_prices):
+        """Helper function which updates the old_prices given new ones
+
+        Args:
+            old_prices (dict): old prices of a given stock code
+            new_prices (dict): new prices of a given stock code
+        """
+        old_prices['open'].insert(0, new_prices['open'])
+        old_prices['close'].insert(0,new_prices['close'])
+        old_prices['high'].insert(0,new_prices['high'])
+        
+        old_prices['open'] = old_prices['open'][:5]
+        old_prices['close'] = old_prices['close'][:5]
+        old_prices['high'] = old_prices['high'][:5]
+        
+    def update_mv_avg(self, code):
+        """Updates the mv avg for a stock code
+
+        Args:
+            code (string): Stock code for the given stock
+        """
+        
+        
+        # new mvg average is equal to sum of the close prices divided by the number of prices stored
+        new_mv_avg = sum(self.get_price(code)['close']) / len(self.get_price(code)['close'])
+        self.set_mv_avg(code, new_mv_avg)
 
     def process_incoming(self, incoming_data):
         for key in incoming_data:
             update_code(str(key), incoming_data[key])
-        return incoming_data
 
     def set_mv_avg(self, code, mv_avg):
         self.mv_avg[code] = mv_avg
